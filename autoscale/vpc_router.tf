e1436c5b7095aa95089bd06dae15470e188360ca
shztki <shztki@gmail.com>
2022-05-27 17:41:56 +0900

    update 2022052707

--- autoscale/vpc_router.tf
+++ autoscale/vpc_router.tf
@@ -1,7 +1,3 @@
-locals {
-  port_forwarding_count = var.server02["count"] + 5
-}
-
 resource "sakuracloud_vpc_router" "vpc_router01" {
   count       = var.vpc_router01["count"]
   name        = format("%s-%s", module.label.id, var.vpc_router01["name"])
